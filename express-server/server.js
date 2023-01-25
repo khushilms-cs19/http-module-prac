b831e2589366bf4ba9740f2f997b519d7cb9ad32
khushilms <khushilms.cs19@bmsce.ac.in>
2023-01-25 15:44:00 +0530

    chore: add server file description

--- express-server/server.js
+++ express-server/server.js
@@ -1,18 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+
+// Routers
 const { taskRouter } = require('./routes/taskRoutes');
 
-
-const app = express();
+// Constants
 const PORT = 3000;
 
+// Initialize app
+const app = express();
+
+// Pre-requisites 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(cors());
 
+
+// Server routes
 app.use('/tasks',taskRouter);
 
+
+// Server running
 app.listen(PORT, ()=>{
   console.log(`Server is running at port: ${PORT}`);
 });
