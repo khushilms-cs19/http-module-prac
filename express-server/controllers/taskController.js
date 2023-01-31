e1e4ba453e59560a194288a6b5b5f9df7e671d0d
khushilms <khushilms.cs19@bmsce.ac.in>
2023-01-31 13:36:05 +0530

    feat: add sequelize and connect with db

--- express-server/controllers/taskController.js
+++ express-server/controllers/taskController.js
@@ -1,91 +1,88 @@
-let tasksList = [
-  {
-    id: 1, 
-    title: 'Buy apples',
-    isCompleted: false,
-  },
-  {
-    id: 2,
-    title: 'Laundry',
-    isCompleted: false,
-  }
-];
-
+const db = require('../database/models');
+const taskServices = require('../services/taskServices');
 
 // get all the tasks 
-const getAllTasks = (req,res) =>{
-  res.status(200).send(tasksList);
+const getAllTasks = async(req,res) =>{
+  const allTasks = await taskServices.getAllTasks();
+  return res.status(200).send(allTasks);
 };
 
 //get a single task based on id
-const getTask = (req,res)=>{
-  const task = tasksList.find((item)=>item.id === Number(req.params.id));
-  if(!task){
-    return res.status(404).send({error: 'Task not found'});
+const getTask = async(req,res)=>{
+  try{
+    const task = await taskServices.getTask(Number(req.params.id));
+    if(!task){
+      return res.status(404).send({error: 'Task not found'});
+    }
+    return res.status(200).send(task);
+  }catch(err){
+    console.log(err);
+    return res.status(500).send({error: 'Something went wrong'});
   }
-  return res.status(200).send(task);
 };
 
 //post a task
-const postTask = (req,res)=>{
-  const task = {
-    id: tasksList.length +1,
-    title: req.body.title,
-    isCompleted: false,
-  };
-  tasksList.push(task);
-  return res.status(201).send(task);
+const postTask = async (req,res)=>{
+  try{
+    const task = await taskServices.postTask(req.body);
+    return res.status(201).send(task);
+  }catch(err){
+    console.log(err);
+    return res.status(500).send({error: 'Something went wrong'});
+  }
 };
 
 // edit single task based on id
-const editTask = (req,res)=>{
+const editTask = async(req,res)=>{
   const taskId = Number(req.params.id);
-  const task = tasksList.find((item)=>item.id === taskId);
-  if(!task){
-    return res.status(404).send({error: 'Task not found'});
-  }
-  task.title = req.body.title;
-  tasksList = tasksList.map((item)=>{
-    if(item.id ===taskId){
-      return task;
+  try {
+    const task = await taskServices.editTask(taskId, req.body);
+    if(!task){
+      return res.status(404).send({error: 'Task not found'});
     }
-    return item;
-  });
-  return res.status(200).send(task);
+    return res.status(200).send(task);
+  }catch(err){
+    console.log(err);
+    return res.status(500).send({error: 'Something went wrong'});
+  }
 };
 
 // set the given task to completed
-const completeTask = (req,res)=>{
-  const taskId = Number(req.params.id);
-  const task = tasksList.find((item)=>item.id === taskId);
-  if(!task){
-    return res.status(404).send({error: 'Task not found'});
-  }
-  task.isCompleted = true;
-  tasksList = tasksList.map((item)=>{
-    if(item.id === taskId){
-      return task;
+const completeTask = async(req,res)=>{
+  try{
+    const taskId = Number(req.params.id);
+    const task = await taskServices.completeTask(taskId);
+    if(!task){
+      return res.status(404).send({error: 'Task not found'});
     }
-    return item;
-  });
-  return res.status(200).send(task);
+    return res.status(200).send(task);
+  }catch(err){
+    console.log(err);
+    return res.status(500).send({error: 'Something went wrong'});
+  }
 };
 
 // delete all the tasks set to completed
-const deleteCompletedTasks = (req,res)=>{
-  tasksList = tasksList.filter((task)=>!task.isCompleted);
-  return res.status(200).send(tasksList);
+const deleteCompletedTasks = async(req,res)=>{
+  try{
+    await taskServices.deleteCompletedTasks();
+    return res.status(200).send({message: 'All completed tasks deleted'});
+  }catch(err){
+    console.log(err);
+    return res.status(500).send({error: 'Something went wrong'});
+  }
 };
 
 // delete a single task based on id
-const deleteTask = (req,res)=>{
-  const taskId = Number(req.params.id);
-  const task = tasksList.find((item)=>item.id === taskId);
-  if(!task){
-    return res.status(404).send({error: 'Task not found'});
+const deleteTask = async(req,res)=>{
+  try{
+    const taskId = Number(req.params.id);
+    await taskServices.deleteTask(taskId);
+    return res.status(200).send({message: 'Task deleted'});
+  }catch(err){
+    console.log(err);
+    return res.status(500).send({error: 'Something went wrong'});
   }
-  tasksList = tasksList.filter((item)=>item.id!==taskId);
-  return res.status(200).send(task);
 };
 
 module.exports = {
---
+++ express-server/services/taskServices.js
@@ -0,0 +1,88 @@
+const db = require('../database/models');
+
+// get all the tasks 
+const getAllTasks = async() =>{
+  const allTasks = await db.task.findAll();
+  return allTasks;
+};
+
+//get a single task based on id
+const getTask = async(taskId)=>{
+  const task = await db.task.findOne({
+    where: {
+      id: taskId,
+    }
+  });
+  return task;
+};
+
+//post a task
+const postTask = async (taskData)=>{
+  const task = {
+    title: taskData.title,
+    isCompleted: false,
+  };
+  db.task.create(task);
+  return task;
+};
+
+// edit single task based on id
+const editTask = async(taskId, taskData)=>{
+  const task = await db.task.findOne({
+    where: {
+      id: taskId,
+    }
+  });
+  if(!task){
+    return null;
+  }
+  task.title = taskData.title;
+  await task.save();
+  return task;
+};
+
+// set the given task to completed
+const completeTask = async(taskId)=>{
+  const task = await db.task.findOne({
+    where: {
+      id: taskId,
+    }
+  });
+  if(!task){
+    return null;
+  }
+  task.isCompleted = true;
+  await task.save();
+  return task;
+};
+
+// delete all the tasks set to completed
+const deleteCompletedTasks = async()=>{
+  await db.task.destroy({
+    where: {
+      isCompleted: true,
+    }
+  });
+  return;
+};
+
+// delete a single task based on id
+const deleteTask = async(taskId)=>{
+  const task = await db.task.findOne({
+    where: {
+      id: taskId,
+    }
+  });
+  await task.destroy();
+  return;
+};
+
+module.exports = {
+  getAllTasks,
+  getTask,
+  postTask,
+  completeTask,
+  deleteCompletedTasks,
+  editTask,
+  deleteTask,
+};
